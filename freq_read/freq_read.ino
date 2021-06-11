bd13611ad2c57b5427d174ee655e5c3471a4e4ae
jacob-armiger <armigersemail@gmail.com>
2021-06-11 14:48:07 -0400

    Refactor to have Pin class and member function

--- freq_read/freq_read.ino
+++ freq_read/freq_read.ino
@@ -1,72 +1,81 @@
 // Print out freq to serial monitor
 
-const int signalPin_2 = 2;     // the number of the interrupt pin
-const int signalPin_3 = 3;
+class Pin {
+  public:
+  /*--CONSTRUCTOR--*/
+  Pin() { state = LOW; freq_time = 0; prog_time = millis(); };
 
-volatile byte state_2 = LOW;    //high,low state of pin 2
-volatile byte state_3 = LOW;    //high,low state of pin 3
+  /*--FUNCTION DECLARATIONS--*/
+  // To be used within the interrupt
+  void measure_PWM();
 
-// PWM measurment
-unsigned long freq_time = 0;
-// Total program time elapsed
-unsigned long prog_time = 0;
-
-// PWM measurment
-unsigned long freq_time_3 = 0;
-// Total program time elapsed
-unsigned long prog_time_3 = 0;
-
-void setup() {
-  // Set buad
-  Serial.begin(9600);
+  /*--VARIABLES--*/
+  // Pin number on board
+  int pin_num;
   
-  // Initialize the signalPins as inputs:
-  pinMode(signalPin_2, INPUT);
-  pinMode(signalPin_3, INPUT);
-  
-  // Attach an interrupt to the ISR for pin 2
-  attachInterrupt(digitalPinToInterrupt(signalPin_2),ISR_2,CHANGE);
-  
-  // Attach an interrupt to the ISR for pin 3
-  attachInterrupt(digitalPinToInterrupt(signalPin_3),ISR_3,CHANGE);
-}
+  // High/low state of pin
+  volatile byte state;
 
-void loop() {
-    Serial.print("thrust: ");
-    Serial.println(freq_time);
-    Serial.print("aux: ");
-    Serial.println(freq_time_3);
+  // PWM measurment
+  unsigned long freq_time;
+  // Total program time elapsed
+  unsigned long prog_time;
 
-    // Loop 10 times every second
-    delay(100);
-}
+};
 
-void ISR_2() {
+void Pin::measure_PWM() {
   noInterrupts();
   // Read whether current signal is high/low
-  state_2 = digitalRead(signalPin_2);
+  state = digitalRead(pin_num);
   
-  if(state_2 == HIGH) {
+  if(state == HIGH) {
     // Measure the time at which state goes to high
     prog_time = micros();
-  } else if(state_2 == LOW) {
+  } else if(state == LOW) {
     // Use the time at which state goes low to get total time state was high
     freq_time = micros() - prog_time;
   }
   interrupts();
 }
 
-void ISR_3() {
-  noInterrupts();
-  // Read whether current signal is high/low
-  state_3 = digitalRead(signalPin_3);
+
+// Globally create pin objects
+Pin pin_2,pin_3;
+
+void setup() {
+  // Set pin numbers
+  pin_2.pin_num = 2;
+  pin_3.pin_num = 3;
   
-  if(state_3 == HIGH) {
-    // Measure the time at which state goes to high
-    prog_time_3 = micros();
-  } else if(state_3 == LOW) {
-    // Use the time at which state goes low to get total time state was high
-    freq_time_3 = micros() - prog_time_3;
-  }
-  interrupts();
+  // Set buad
+  Serial.begin(9600);
+  
+  // Initialize the signalPins as inputs:
+  pinMode(pin_2.pin_num, INPUT);
+  pinMode(pin_3.pin_num, INPUT);
+  
+  // Attach an interrupt to the ISR for pin 2
+  attachInterrupt(digitalPinToInterrupt(pin_2.pin_num),ISR_2,CHANGE);
+  
+  // Attach an interrupt to the ISR for pin 3
+  attachInterrupt(digitalPinToInterrupt(pin_3.pin_num),ISR_3,CHANGE);
+}
+
+void loop() {
+    Serial.print("Thrust: ");
+    Serial.println(pin_2.freq_time);
+    
+    Serial.print("Aux1: ");
+    Serial.println(pin_3.freq_time);
+
+    // Loop 10 times every second
+    delay(100);
+}
+
+void ISR_2() {
+  pin_2.measure_PWM();
+}
+
+void ISR_3() {
+  pin_3.measure_PWM();
 }
